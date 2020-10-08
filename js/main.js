5ff76d2446da6bf9985808dcff579d703dae7773
Arthur <artur.20.03@mail.ru>
2020-10-08 12:57:29 +0700

    Добавил рандом в данные, let на const и переименовал data в pinsData

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 'use strict';
-const COUNT_OF_OBJECTS = 8;
+const COUNT_OF_PINS = 8;
 const TYPES = [`palace`, `flat`, `house`, `bungalow`];
 const TIMES = [`12:00`, `13:00`, `14:00`];
 const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 const PHOTOS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+const MIN_ROOMS = 1;
+const MAX_ROOMS = 7;
+const MIN_PRICE = 1000;
+const MAX_PRICE = 9999;
+const MIN_GUESTS = 1;
+const MAX_GUESTS = 15;
+const MIN_X_LOCATION = 0;
 const MAX_X_LOCATION = document.querySelector(`.map`).offsetWidth;
+const TITLES = [`first`, `twice`, `third`, `quatro`];
+const DESCRIPTION = [`good rooms`, `bad rooms`, `nice`, `fine`, `not good`];
+const MIN_Y_LOCATION = 130;
+const MAX_Y_LOCATION = 630;
 
-
-let data = getData(COUNT_OF_OBJECTS);
-createPin(data);
-
-function createPin(someData) {
-  let fragment = document.createDocumentFragment();
-  someData.forEach((object) => fragment.appendChild(generatePin(object)));
-  let mapPins = document.querySelector(`.map__pins`);
+function createPin(pinsData) {
+  const fragment = document.createDocumentFragment();
+  pinsData.forEach((object) => fragment.appendChild(generatePin(object)));
+  const mapPins = document.querySelector(`.map__pins`);
   mapPins.appendChild(fragment);
 }
 
@@ -23,43 +30,55 @@ function getRandomItem(array) {
 function getRandomNumber(min, max) {
   return Math.round(Math.random() * (max - min)) + min;
 }
+function getRandomSlice(array) {
+  const maxCount = array.length - 1;
+  return array.slice(0, getRandomNumber(0, maxCount));
+}
+function getPinsData(countOfObjects) {
+  const pinsData = [];
 
-function getData(countOfObjects) {
-
-  let someData = [];
   for (let i = 0; i < countOfObjects; i++) {
-    someData.push({
+
+    const locationX = getRandomNumber(MIN_X_LOCATION, MAX_X_LOCATION);
+    const locationY = getRandomNumber(MIN_Y_LOCATION, MAX_Y_LOCATION);
+
+    const pin = {
       "author": {
         "avatar": `img/avatars/user0${i + 1}.png`
       },
       "offer": {
-        "title": `строка, заголовок предложения`,
-        "address": `600, 350`,
-        "price": `1200`,
+        "title": getRandomItem(TITLES),
+        "address": `${locationX}, ${locationY}`,
+        "price": getRandomNumber(MIN_PRICE, MAX_PRICE),
         "type": getRandomItem(TYPES),
-        "rooms": 21,
-        "guests": 210,
+        "rooms": getRandomNumber(MIN_ROOMS, MAX_ROOMS),
+        "guests": getRandomNumber(MIN_GUESTS, MAX_GUESTS),
         "checkin": getRandomItem(TIMES),
         "checkout": getRandomItem(TIMES),
-        "features": getRandomItem(FEATURES),
-        "description": `строка с описанием`,
-        "photos": getRandomItem(PHOTOS),
+        "features": getRandomSlice(FEATURES),
+        "description": getRandomItem(DESCRIPTION),
+        "photos": getRandomSlice(PHOTOS),
       },
       "location": {
-        "x": getRandomNumber(0, MAX_X_LOCATION),
-        "y": getRandomNumber(130, 630)
+        "x": locationX,
+        "y": locationY
       }
-    });
+    };
+    pinsData.push(pin);
   }
-  return someData;
+  return pinsData;
 }
 
-function generatePin(someObject) {
-  let templateOrigin = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
-  let myTemplate = templateOrigin.cloneNode(true);
-  myTemplate.querySelector(`img`).src = someObject.author.avatar;
-  myTemplate.querySelector(`img`).alt = someObject.offer.title;
-  myTemplate.style.left = `${someObject.location.x}px`;
-  myTemplate.style.top = `${someObject.location.y}px`;
+function generatePin(pinObject) {
+  const templateOrigin = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+  const myTemplate = templateOrigin.cloneNode(true);
+  myTemplate.querySelector(`img`).src = pinObject.author.avatar;
+  myTemplate.querySelector(`img`).alt = pinObject.offer.title;
+  myTemplate.style.left = `${pinObject.location.x}px`;
+  myTemplate.style.top = `${pinObject.location.y}px`;
   return myTemplate;
 }
+
+
+const pinsData = getPinsData(COUNT_OF_PINS);
+createPin(pinsData);
